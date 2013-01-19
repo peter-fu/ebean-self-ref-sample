84544b48df24948411864015a3393f212c78849c
Peter Fu <peter.fu@activecollabmod.com>
2013-01-20 03:18:41 +0800

    add sample about additional-sql

--- README.md
+++ README.md
@@ -9,3 +9,17 @@ Usage
 Simply run the app then access http://localhost:9000, the following json string will show up indicating that the third customer information is not fully loaded (it's only a reference).
 
 [{"id":3,"name":"baz","referredBy":{"id":1}},{"id":2,"name":"Bar","referredBy":{"id":1}},{"id":1}]
+
+Additional SQL
+
+Access http://localhost:9000/additional-sql and watch the output from console, there is an additional SQL executed when trying to access Customer(2)'s name.
+
+[debug] application - Application.additionalSql
+[debug] c.j.b.PreparedStatementHandle - select t0.id c0, t0.name c1, t0.referred_by_id c2 
+from customer t0 
+order by t0.id desc
+[debug] application - Accessing customer(2)'s name
+[debug] c.j.b.PreparedStatementHandle - select t0.id c0, t0.name c1, t0.referred_by_id c2 
+from customer t0
+where t0.id = 2  
+[debug] application - Before converting to JSON
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.List;
 
 import models.Customer;
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -11,7 +12,18 @@ import com.avaje.ebean.Ebean;
 public class Application extends Controller {
 
     public static Result index() {
+        Logger.debug("Application.index");
         List<Customer> customers = Customer.find.orderBy("id desc").findList();
+        Logger.debug("Before converting to JSON");
+        return ok(Ebean.createJsonContext().toJsonString(customers));
+    }
+
+    public static Result additionalSql() {
+        Logger.debug("Application.additionalSql");
+        List<Customer> customers = Customer.find.orderBy("id desc").findList();
+        Logger.debug("Accessing customer(2)'s name");
+        String name = customers.get(2).name;
+        Logger.debug("Before converting to JSON");
         return ok(Ebean.createJsonContext().toJsonString(customers));
     }
 
